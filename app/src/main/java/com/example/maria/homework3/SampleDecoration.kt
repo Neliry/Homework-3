cce310a364f3aafb08be26794ab49b5013f277d9
Rudakova Mariya <mariyars11@gmail.com>
2018-11-13 16:28:12 +0200

    Small design change

--- app/src/main/java/com/example/maria/homework3/SampleDecoration.kt
+++ app/src/main/java/com/example/maria/homework3/SampleDecoration.kt
@@ -20,7 +20,7 @@ internal class SampleDecoration(context: Context): RecyclerView.ItemDecoration()
         val position = parent.getChildAdapterPosition(view)
         val viewType = parent.adapter!!.getItemViewType(position)
         when (viewType) {
-            0 -> outRect.set(0.px, 16.px, 0.px, 16.px)
+            0 -> outRect.set(16.px, 16.px, 16.px, 16.px)
             1 -> outRect.set(36.px, 28.px, 120.px, 28.px)
             2 -> outRect.set(120.px, 28.px, 36.px, 28.px)
         }
@@ -35,12 +35,12 @@ internal class SampleDecoration(context: Context): RecyclerView.ItemDecoration()
             when (viewType) {
                 0 -> {
                     mPaint.color = Color.parseColor("#90A4AE")
-                    c.drawRect(view.left.toFloat()-16.px, view.bottom.toFloat()+16.px, view.right.toFloat()+16.px, (view.bottom - view.height-16.px).toFloat(), mPaint)
+                    c.drawRect(view.left.toFloat()-32.px, view.bottom.toFloat()+16.px, view.right.toFloat()+32.px, (view.bottom - view.height-16.px).toFloat(), mPaint)
                 }
                 1 -> {
-                    mPaint.color = Color.parseColor("#f5964e")
+                    mPaint.color = Color.parseColor("#4527A0")
                     c.drawOval(view.left.toFloat()-23.px, view.bottom.toFloat()+23.px, view.right.toFloat()+23.px, (view.bottom - view.height-23.px).toFloat(), mPaint)
-                    mPaint.color = Color.parseColor("#ffb15e")
+                    mPaint.color = Color.parseColor("#B39DDB")
                     c.drawOval(view.left.toFloat()-20.px, view.bottom.toFloat()+20.px, view.right.toFloat()+20.px, (view.bottom - view.height-20.px).toFloat(), mPaint)
                 }
                 2 -> {
@@ -50,9 +50,9 @@ internal class SampleDecoration(context: Context): RecyclerView.ItemDecoration()
                     width = if(tv.width>et.width){
                         tv.width
                     } else et.width
-                    mPaint.color = Color.parseColor("#389aeb")
+                    mPaint.color = Color.parseColor("#0097A7")
                     c.drawOval(view.right.toFloat()-23.px-width, view.bottom.toFloat()+23.px, view.right.toFloat()+23.px, (view.bottom - view.height-23.px).toFloat(), mPaint)
-                    mPaint.color = Color.parseColor("#5cb6ff")
+                    mPaint.color = Color.parseColor("#80DEEA")
                     c.drawOval(view.right.toFloat()-20.px-width, view.bottom.toFloat()+20.px, view.right.toFloat()+20.px, (view.bottom - view.height-20.px).toFloat(), mPaint)
                 }
             }
